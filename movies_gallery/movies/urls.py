bed3cf07d8acea7a33408540fa243d416c6854f9
Ars44 <Hrieleght2>
2021-07-21 21:47:25 +0300

    add application (+ needed folders) add models into movies registered models in admin panel created superuser for admin panel translated admin panel into russian created records via admin panel create new directore 'media' (to restore ihages here) add views add template add django.urls.include usage create new dir 'static' add front-end

--- movies_gallery/movies/urls.py
+++ movies_gallery/movies/urls.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
-
+from django.urls import path
+from . import views
 
 class MoviesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'movies'
+
+urlpatterns = [
+    path("", views.MoviesView.as_view()),
+    # path("<slug:slug>/", views.MovieDetailView.as_view(), name="movie_detail"),
+    # path("review/<int:pk>/", views.AddReview.as_view(), name="add_review"),
+]
--- movies_gallery/movies/views.py
+++ movies_gallery/movies/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView
+from django.views.generic.base import View
 
-# Create your views here.
+from .models import Movie
+
+
+class MoviesView(View):
+    """Список фильмов"""
+    def get(self, request):
+        movies = Movie.objects.all()
+        return render(request, "movies/movies.html", {'movie_list': movies})
+
+
+# class MovieDetailView(DetailView):
+#     """Полное описание фильма"""
+#     model = Movie
+#     slug_field = "url"
+#
+#
+# class AddReview(View):
+#     """Отзывы"""
+#     def post(self, request, pk):
+#         form = ReviewForm(request.POST)
+#         movie = Movie.objects.get(id=pk)
+#         if form.is_valid():
+#             form = form.save(commit=False)
+#             form.movie = movie
+#             form.save()
+#         return redirect(movie.get_absolute_url())
